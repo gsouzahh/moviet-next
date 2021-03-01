9b3d6ba40b7c373e6c083fe1e9e5949250215163
gsouzahh <73115455+gsouzahh@users.noreply.github.com>
2021-03-01 00:32:48 -0300

    more

--- Moviet/pages/index.tsx
+++ Moviet/pages/index.tsx
@@ -1,9 +1,26 @@
 import { ExperienceBar } from "../public/components/ExperienceBar";
+import { CompletedChallanger } from "../src/components/CompletedChallanger";
+import { Countdown } from "../src/components/Countdown";
+import { Profile } from "../src/components/Profile";
+import styles from '../src/styles/pages/Home.module.css';
+
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Início | NLW</title>
+      </Head>
       <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallanger />
+          <Countdown />
+        </div>
+        <div></div>
+      </section>
     </div>
   );
 }
--- Moviet/src/components/Countdown.tsx
+++ Moviet/src/components/Countdown.tsx
@@ -3,9 +3,12 @@ import style from "../styles/components/Countdown.module.css";
 
 export function Countdown() {
   const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
+
+  let countdownTimeout: NodeJS.Timeout;
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
   const [secondsLeft, secondsRight] = String(seconds)
@@ -13,15 +16,25 @@ export function Countdown() {
     .split("");
 
   function CountDown() {
-    setActive(true);
+    setIsActive(true);
   }
+
+  function ResetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(25 * 60);
+  }
+
   useEffect(() => {
-    if (active && time > 0) {
-      setTimeout(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
     }
-  }, [active, time]);
+  }, [isActive, time]);
 
   return (
     <div>
@@ -36,13 +49,34 @@ export function Countdown() {
           <span>{secondsRight}</span>
         </div>
       </div>
-      <button
-        type="button"
-        className={style.countDownButton}
-        onClick={CountDown}
-      >
-        Iniciar um ciclo
-      </button>
+      { hasFinished ? (
+        <button
+          disabled
+          className={style.countDownButton}
+        >
+          Ciclo Finalizado
+        </button>
+      ) : (
+          <>
+            {isActive ? (
+              <button
+                type="button"
+                className={`${style.countDownButton} ${style.countDownButtonActive}`}
+                onClick={ResetCountdown}
+              >
+                Abandonar o ciclo
+              </button>
+            ) : (
+                <button
+                  type="button"
+                  className={style.countDownButton}
+                  onClick={CountDown}
+                >
+                  Iniciar um ciclo
+                </button>
+              )}
+          </>
+        )}
     </div>
   );
 }
